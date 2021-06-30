58105502e831701dab61bd7dd8d40c6c9b856f6e
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-30 20:03:38 +0700

    update ui

--- app/src/main/java/com/example/stringresource28042021/MainActivity.java
+++ app/src/main/java/com/example/stringresource28042021/MainActivity.java
@@ -3,10 +3,17 @@ package com.example.stringresource28042021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTvInfo;
+    TextInputEditText mInputEmail,mInputPassword;
+    Button mBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
