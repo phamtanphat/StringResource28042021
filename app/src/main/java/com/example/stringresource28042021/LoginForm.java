0ee2eead8728ae627108abf77fcd1efee731cd66
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-30 21:16:50 +0700

    databinding

---
+++ app/src/main/java/com/example/stringresource28042021/LoginForm.java
@@ -0,0 +1,14 @@
+package com.example.stringresource28042021;
+
+public class LoginForm {
+    public String email;
+    public String password;
+
+    public LoginForm(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public LoginForm() {
+    }
+}
--- app/src/main/java/com/example/stringresource28042021/MainActivity.java
+++ app/src/main/java/com/example/stringresource28042021/MainActivity.java
@@ -1,24 +1,74 @@
 package com.example.stringresource28042021;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.stringresource28042021.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mTvInfo;
-    TextInputEditText mInputEmail,mInputPassword;
-    Button mBtnLogin;
-
+    ActivityMainBinding mBinding;
+    MutableLiveData<LoginForm> mMutableLiveData;
+    LoginForm loginForm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        loginForm = new LoginForm("","");
+        mMutableLiveData = new MutableLiveData<>();
+        mBinding.setMutable(mMutableLiveData);
+        mBinding.setLifecycleOwner(this);
+
+        mBinding.textInputEditTextMail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                loginForm.email = String.valueOf(charSequence);
+                mMutableLiveData.setValue(loginForm);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mBinding.textInputEditTextPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                loginForm.password = String.valueOf(charSequence);
+                mMutableLiveData.setValue(loginForm);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mBinding.buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
