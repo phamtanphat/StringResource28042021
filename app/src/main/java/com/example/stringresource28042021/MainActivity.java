4d4d710e451bf79360dc6ce4a140ec7766aece27
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-02 20:28:52 +0700

    cach 2 set language app

--- app/src/main/java/com/example/stringresource28042021/MainActivity.java
+++ app/src/main/java/com/example/stringresource28042021/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.stringresource28042021;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Toast;
 
@@ -9,12 +14,27 @@ import androidx.databinding.DataBindingUtil;
 
 import com.example.stringresource28042021.databinding.ActivityMainBinding;
 
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding mBinding;
+    SharedPreferences mSharedPreferences;
+    SharedPreferences.Editor mEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSharedPreferences = getSharedPreferences("mycache",MODE_PRIVATE);
+
+        if (mSharedPreferences != null && getLocal().length() > 0){
+            Locale myLocale = new Locale(getLocal());
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+        }
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
         mBinding.buttonSignIn.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +49,42 @@ public class MainActivity extends AppCompatActivity {
                 mBinding.textViewInfo.setText(String.format("%s : %s \r\n%s : %s",labelEmail,email,labelPassword,password));
             }
         });
+
+        mBinding.imageLanguageVn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLocale("vi");
+            }
+        });
+        mBinding.imageLanguageEn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setLocale("en");
+            }
+        });
+    }
+
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, MainActivity.class);
+        finish();
+        startActivity(refresh);
+        saveLocal(lang);
+    }
+    private String getLocal(){
+        return mSharedPreferences.getString("local","");
+    }
+    private void saveLocal(String local){
+        if (mSharedPreferences != null){
+            mEditor = mSharedPreferences.edit();
+            mEditor.putString("local",local);
+            mEditor.apply();
+        }
 
     }
 }
